614213c5be8ab330b731241545d09c4d5dc0ac7e
Nimeth Fernando <108478292+nimeth42@users.noreply.github.com>
2024-07-26 09:42:26 +0530

    Edit changers

--- server/index.js
+++ server/index.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 
 app.listen(3001, () => {
-    console.log("server up and runn9ing on port 3")
+    console.log("server up and runn9ing on port 3001")
 });
